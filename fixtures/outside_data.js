e8a62bf812ea395d9bc3ae7a7ee22db174fe4c5e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-06 12:03:46 -0400

    build server, pass in envvar, and check js

---
+++ fixtures/outside_data.js
@@ -0,0 +1,10 @@
+
+var outside_data = [
+  {
+    id: "data",
+    name: "Data",
+    desc: { separator:"\t", primaryKey:"id", columns:[{"column": "a", "type": "string"}, {"column": "b", "type": "string"}, {"column": "c", "type": "string"}, {"column": "x", "type": "string"}, {"column": "y", "type": "string"}, {"column": "z", "type": "string"}] },
+    url: "data:text/plain;charset=utf-8,a%09b%09c%09x%09y%09z%0A1%092%093%09%09%09%0A7%098%099%09%09%09%0A%09%09%091%092%093"
+  }
+];
+
