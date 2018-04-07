017baad8b373480970cf6ebbb26df65ff737693a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-06 22:33:00 -0400

    fix bug

--- fixtures/expected-outside_data.js
+++ fixtures/expected-outside_data.js
@@ -3,7 +3,7 @@ var outside_data = [
   {
     id: "data",
     name: "Data",
-    desc: { separator:"\t", primaryKey:"id", columns:[{"column": "a", "type": "string"}, {"column": "b", "type": "string"}, {"column": "c", "type": "string"}, {"column": "x", "type": "string"}, {"column": "y", "type": "string"}, {"column": "z", "type": "string"}] },
+    desc: { separator:"\t", primaryKey:"id", columns:[{"column": "a", "type": "number"}, {"column": "b", "type": "number"}, {"column": "c", "type": "number"}, {"column": "x", "type": "number"}, {"column": "y", "type": "number"}, {"column": "z", "type": "number"}] },
     url: "data:text/plain;charset=utf-8,a%09b%09c%09x%09y%09z%0A1%092%093%09%09%09%0A7%098%099%09%09%09%0A%09%09%091%092%093"
   }
 ];
